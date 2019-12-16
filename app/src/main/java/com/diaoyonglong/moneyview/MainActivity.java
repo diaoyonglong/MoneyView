157326cd95e3368232601cddb1fa14483eb3b48b
diaoyonglong <diaoyonglong@aoji.cn>
2019-12-16 15:54:27 +0800

    使用示例

--- app/src/main/java/com/diaoyonglong/moneyview/MainActivity.java
+++ app/src/main/java/com/diaoyonglong/moneyview/MainActivity.java
@@ -3,11 +3,23 @@ package com.diaoyonglong.moneyview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.diaoyonglong.moneyviewlib.view.MoneyView;
+
+/**
+ * Created by diaoyonglong on 2019/12/13
+ *
+ * @desc 样例
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private MoneyView mPriceView;//金额
+    private MoneyView mZheView;//折扣
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPriceView = findViewById(R.id.money_view_price);
+        mZheView = findViewById(R.id.money_view_zhe);
     }
 }
